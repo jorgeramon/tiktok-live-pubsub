e4b1ae50ab8f75b0a68bb3a6693c8f696b7ff1af
Jorge Ramon <jorge.ramon2128@gmail.com>
2025-05-29 16:34:57 -0600

    RoomInfo is optional in interface

--- src/interfaces/connector-online-status-message.ts
+++ src/interfaces/connector-online-status-message.ts
@@ -3,5 +3,5 @@ import { IConnectorOnlineMessage } from '@/interfaces/connector-online-message';
 export interface IConnectorOnlineStatusMessage {
   owner_username: string;
   is_online: boolean;
-  room_info: IConnectorOnlineMessage;
+  room_info?: IConnectorOnlineMessage;
 }
